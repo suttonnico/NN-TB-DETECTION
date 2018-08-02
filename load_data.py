cf39e4459274b199c3240ec046566affc318e7db
NEGU93 <joseagustin.barra@gmail.com>
2018-08-02 19:37:32 -0300

    Save pickled data to make it faster

--- load_data.py
+++ load_data.py
@@ -1,10 +1,10 @@
 import numpy as np
 import os
 import cv2
-from sklearn import decomposition
+import pickle
 
 
-def get_labels(labels_dir='data/ChinaSet_AllFiles/ClinicalReadings/', n=662):
+def save_labels(labels_dir='data/ChinaSet_AllFiles/ClinicalReadings/', n=662):
     labels = np.zeros(n)
     labels_files = [f for f in os.listdir(labels_dir)]
     for f in labels_files:
@@ -18,21 +18,35 @@ def get_labels(labels_dir='data/ChinaSet_AllFiles/ClinicalReadings/', n=662):
                     flag = 1                                    # flag = 1
             if flag == 0:
                 labels[int(f[7:11])-1] = 1                      # label = 1 -> PTB
-    return labels
+    with open("data/labels", 'wb') as fp:
+        pickle.dump(labels, fp)
 
 
-def get_images(imgs_dir='data/ChinaSet_AllFiles/CXR_png/'):
+def save_images(imgs_dir='data/ChinaSet_AllFiles/CXR_png/'):
     extensions = {".jpg", ".png", ".gif"}  # etc
     # file: any(file.endswith(ext) for ext in extensions) for file in files
     i = 0
     # make sure the file is a image
     imgs_files = [f for f in os.listdir(imgs_dir) if any(f.endswith(ext) for ext in extensions)]
-    images = np.zeros((len(imgs_files), 290, 300))
+    images = []
     for f in imgs_files:
         # print(f)
         img = cv2.imread(os.path.join(imgs_dir, f), cv2.IMREAD_GRAYSCALE)        # img.shape ~ (2919, 3000)
-        # print(img.shape)
-        img = cv2.resize(img, (300, 290))
-        images[i] = img     # Reshape images TODO: Elegir éste valor de resize acorde
-        i += 1
-    return images
+        img = cv2.resize(img, (300, 300))
+        images.append(img)     # Reshape images TODO: Elegir éste valor de resize acorde
+    with open("data/images", 'wb') as fp:
+        pickle.dump(images, fp)
+
+
+def get_labels():
+    if not os.path.exists("data/labels"):
+        save_labels()
+    with open("data/labels", 'rb') as fp:
+        return pickle.load(fp)
+
+
+def get_images():
+    if not os.path.exists("data/images"):
+        save_images()
+    with open("data/images", 'rb') as fp:
+        return pickle.load(fp)
--- main.py
+++ main.py
@@ -5,10 +5,6 @@ import cv2
 def main():
     labels = ld.get_labels()
     img = ld.get_images()
-    cv2.imshow(str(labels[0]), img[0])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    import pdb; pdb.set_trace()
 
 if __name__ == "__main__":
     main()
