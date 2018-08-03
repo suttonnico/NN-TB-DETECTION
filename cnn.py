ebdb71d47b622f9e59c7673d1e78d32f6dfeb9fb
NEGU93 <joseagustin.barra@gmail.com>
2018-08-03 18:59:22 -0300

    Plotting done

--- cnn.py
+++ cnn.py
@@ -1,28 +1,74 @@
 from keras import layers, models, optimizers
+import sklearn
+import itertools
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def plot_val_acc(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    plt.plot(epochs, acc, 'blue', label='Training acc')
+    plt.plot(epochs, val_acc, 'red', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'blue', label='Training loss')
+    plt.plot(epochs, val_loss, 'red', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    plt.figure(figsize=(5, 5))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
 
 
 def cnn(img_width=128, img_height=128):
     # https://www.kaggle.com/crawford/lung-infiltration-cnn-with-keras-on-chest-x-rays
     model = models.Sequential()
 
-    model.add(layers.Conv1D(32, 3, input_shape=(img_width, img_height)))
+    model.add(layers.Conv2D(32, (3, 3), input_shape=(img_width, img_height, 1)))  # the 1 is because greyscale (1 chan)
     model.add(layers.BatchNormalization())
     model.add(layers.Activation("relu"))
     # TODO: DROPOUT acá también ponemos?
 
-    model.add(layers.MaxPooling1D(2))
+    model.add(layers.MaxPooling2D((2, 2)))
 
-    model.add(layers.Conv1D(64, 3))
+    model.add(layers.Conv2D(64, (3, 3)))
     model.add(layers.BatchNormalization())
     model.add(layers.Activation("relu"))
 
-    model.add(layers.MaxPooling1D(2))
+    model.add(layers.MaxPooling2D((2, 2)))
 
-    model.add(layers.Conv1D(128,  3))
+    model.add(layers.Conv2D(128, (3, 3)))
     model.add(layers.BatchNormalization())
     model.add(layers.Activation("relu"))
 
-    model.add(layers.MaxPooling1D(2))
+    model.add(layers.MaxPooling2D((2, 2)))
 
     model.add(layers.Flatten())
     model.add(layers.Dropout(0.2))
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from keras.callbacks import ModelCheckpoint
+from sklearn.metrics import confusion_matrix
 import load_data as ld
 import numpy as np
 import cnn
+
+dict_characters = {0: 'Normal', 1: 'PTB'}
 
 # TODO: IDEAS:
 # Subir contraste!
@@ -9,14 +12,19 @@ import cnn
 # PCA o algo
 
 
+def reshape(array):
+    res = np.asarray(array)
+    return res.reshape(res.shape + (1,))
+
+
 def main():
     print("Opening data & labels")
     train_set, train_labels = ld.get_train_set(img_width=128, img_height=128)
     test_set, test_labels = ld.get_test_set(img_width=128, img_height=128)
-    test_set = np.asarray(test_set)
-    test_labels = np.asarray(test_labels)
-    train_labels = np.asarray(train_labels)
-    train_set = np.asarray(train_set)
+    test_set = reshape(test_set)
+    test_labels = reshape(test_labels)
+    train_labels = reshape(train_labels)
+    train_set = reshape(train_set)
     # import pdb; pdb.set_trace()
     print("Trining Net")
     epochs = 10
@@ -27,15 +35,21 @@ def main():
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     callbacks_list = [checkpoint]
     # train
-    my_cnn.fit(x=train_set,             # Input should be (train_cases, 128, 128, 1)
-               y=train_labels,
-               batch_size=batch_size,
-               epochs=epochs,
-               verbose=2,
-               callbacks=callbacks_list,
-               validation_data=(test_set, test_labels)
-               )
-    print("Done Training...")
+    history = my_cnn.fit(x=train_set,             # Input should be (train_cases, 128, 128, 1)
+                         y=train_labels,
+                         batch_size=batch_size,
+                         epochs=epochs,
+                         verbose=2,
+                         callbacks=callbacks_list,
+                         validation_data=(test_set, test_labels)
+                         )
+    print("Plot results")
+    cnn.plot_val_acc(history=history)
+    # Confusion Matrix
+    Y_pred = my_cnn.predict_classes(test_set)
+    Y_pred_classes = np.argmax(Y_pred, axis=1)
+    confusion_mtx = confusion_matrix(test_labels, Y_pred_classes)
+    cnn.plot_confusion_matrix(confusion_mtx, classes=list(dict_characters.values()))
 
 
 if __name__ == "__main__":
