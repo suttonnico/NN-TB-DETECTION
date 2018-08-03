9f3240dc1e7359c6e66f6d821ec6b8f5c4555271
NEGU93 <joseagustin.barra@gmail.com>
2018-08-03 16:08:43 -0300

    Techincally the code done but it was too fast to be true

--- cnn.py
+++ cnn.py
@@ -5,24 +5,24 @@ def cnn(img_width=128, img_height=128):
     # https://www.kaggle.com/crawford/lung-infiltration-cnn-with-keras-on-chest-x-rays
     model = models.Sequential()
 
-    model.add(layers.Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3)))
+    model.add(layers.Conv1D(32, 3, input_shape=(img_width, img_height)))
     model.add(layers.BatchNormalization())
     model.add(layers.Activation("relu"))
     # TODO: DROPOUT acá también ponemos?
 
-    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.MaxPooling1D(2))
 
-    model.add(layers.Conv2D(64, (3, 3)))
+    model.add(layers.Conv1D(64, 3))
     model.add(layers.BatchNormalization())
     model.add(layers.Activation("relu"))
 
-    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.MaxPooling1D(2))
 
-    model.add(layers.Conv2D(128, (3, 3)))
+    model.add(layers.Conv1D(128,  3))
     model.add(layers.BatchNormalization())
     model.add(layers.Activation("relu"))
 
-    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.MaxPooling1D(2))
 
     model.add(layers.Flatten())
     model.add(layers.Dropout(0.2))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from keras.callbacks import ModelCheckpoint
 import load_data as ld
-import cv2
-import random
-# import cnn
+import numpy as np
+import cnn
 
 # TODO: IDEAS:
 # Subir contraste!
@@ -11,11 +11,31 @@ import random
 
 def main():
     print("Opening data & labels")
-    train_set, train_labels = ld.get_train_set(img_width=300, img_height=300)
-    test_set, test_labels = ld.get_test_set(img_width=300, img_height=300)
-    import pdb; pdb.set_trace()
+    train_set, train_labels = ld.get_train_set(img_width=128, img_height=128)
+    test_set, test_labels = ld.get_test_set(img_width=128, img_height=128)
+    test_set = np.asarray(test_set)
+    test_labels = np.asarray(test_labels)
+    train_labels = np.asarray(train_labels)
+    train_set = np.asarray(train_set)
+    # import pdb; pdb.set_trace()
     print("Trining Net")
-    # my_cnn = cnn.cnn(img_width=300, img_height=300)
+    epochs = 10
+    batch_size = 16
+    my_cnn = cnn.cnn(img_width=128, img_height=128)
+    # checkpoint
+    filepath = "out/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    callbacks_list = [checkpoint]
+    # train
+    my_cnn.fit(x=train_set,             # Input should be (train_cases, 128, 128, 1)
+               y=train_labels,
+               batch_size=batch_size,
+               epochs=epochs,
+               verbose=2,
+               callbacks=callbacks_list,
+               validation_data=(test_set, test_labels)
+               )
+    print("Done Training...")
 
 
 if __name__ == "__main__":
